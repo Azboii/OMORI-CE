306960d4bf9446295b3055ff535f47d56b5b77c3
Celesteisscool <105241545+Azboii@users.noreply.github.com>
2023-04-14 14:50:52 -0400

    Update main.cpp
    Add working menu (Settings is w.i.p.)
    Fix bug when pressing two opposite directions at once

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include <graphx.h>
 #include <sys/rtc.h>
+#include <keypadc.h>
 
 #include "utils.h"
 #include "title.h"
@@ -20,36 +21,79 @@ int main(void)
     int x = 0;
     int y = 0;
 
-    uint8_t seconds = 0;
+    int seconds = 0;
+    
+    int menu = 1;
+    bool menu_lock = false;
+    int selected = 0;
+    int Hand_pos = 135;
 
     rtc_Enable(RTC_SEC_INT);
 
-    Utils::drawTitleScreen(0);
+    Utils::drawTitleScreen(0,Hand_pos);
 
     while (rtc_IsBusy());
 
     rtc_AckInterrupt(RTC_INT_MASK);
 
-    do {
+    MENU:do {
 
         if (rtc_ChkInterrupt(RTC_SEC_INT))
         {
             seconds++;
-
+            if (seconds > 3) {
+                seconds = 1;
+            }
             rtc_AckInterrupt(RTC_SEC_INT);
         }
 
-        if (seconds == 1)
-            Utils::drawTitleScreen(0);
-        else if (seconds == 2)
-            Utils::drawTitleScreen(1);
-        else if (seconds == 3)
-        {
-            Utils::drawTitleScreen(2);
-            seconds = 0;
+        if (seconds == 1) {
+            Utils::drawTitleScreen(0,Hand_pos);
+        } else if (seconds == 2) {
+            Utils::drawTitleScreen(1,Hand_pos);
+        } else if (seconds == 3) {
+            Utils::drawTitleScreen(2,Hand_pos);
+        }
+        
+        if (!Utils::getButtonDown(0) && !Utils::getButtonDown(1)) {
+            menu_lock = false;
         }
 
-    } while (kb_Data[1] != kb_2nd);
+        if (Utils::getButtonDown(0) && !Utils::getButtonDown(1) && !menu_lock) {
+            menu--; 
+            menu_lock = true;
+        } else if (Utils::getButtonDown(1) && !Utils::getButtonDown(0) && !menu_lock) {
+            menu++; 
+            menu_lock = true;
+        }
+
+        if (menu < 0) {
+            menu = 2;
+        }
+        if (menu > 2) {
+            menu = 0;
+        }
+
+        if (menu == 0) {
+            Hand_pos = 25 + 100 * menu;
+        } else {
+            Hand_pos = 35 + 100 * menu;
+        }
+        
+        bool key = false;
+        bool prevkey = false;
+        key = kb_Data[1] == kb_2nd;
+
+        if (key != prevkey) {
+            selected = (menu + 1);
+        }
+        prevkey = key;
+
+    } while (selected == 0);
+
+    if (selected == 3) {
+        goto MENU;
+    }
 
     // while (rtc_IsBusy());
 
@@ -71,22 +115,20 @@ int main(void)
 
         kb_Scan();
 
-        if (Utils::getButtonDown(0))
+        if (Utils::getButtonDown(0) && !Utils::getButtonDown(1))
         {
             x--; // Left
             Utils::drawCharacter(x, y, None);
-        }
-        if (Utils::getButtonDown(1))
-        {
+        } else if (Utils::getButtonDown(1) && !Utils::getButtonDown(0)) {
             x++; // Right
             Utils::drawCharacter(x, y, None);
         }
-        if (Utils::getButtonDown(2))
+
+        if (Utils::getButtonDown(2) && !Utils::getButtonDown(3))
         {
             y--; // Up
             Utils::drawCharacter(x, y, None);
-        }
-        if (Utils::getButtonDown(3))
+        } else if (Utils::getButtonDown(3) && !Utils::getButtonDown(2))
         {
             y++; // Down
             Utils::drawCharacter(x, y, Down);
